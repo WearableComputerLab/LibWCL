719086a2f5e82e17e506a1ca621f51a645e62d55
Aaron Stafford <aaron.stafford@unisa.edu.au>
2006-06-05 04:36:19 +0000

    I renamed these files from IEEE1394cam.

---
+++ src/wcl/1394cam/IEEE1394Cam.cpp
@@ -0,0 +1,18 @@
+#include "IEEE1394Cam.h"
+
+// constructor
+IEEE1394Cam::IEEE1394Cam()
+{
+
+}
+
+// method to retrieve an image from the camera.
+unsigned char* IEEE1394Cam::getImage()
+{
+	return 0;
+}
+
+// destructor
+IEEE1394Cam::~IEEE1394Cam()
+{
+}
