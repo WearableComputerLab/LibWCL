6ee6271f082c3aff6ffcc557dc7480b3669ebeb9
Aaron Stafford <aaron.stafford@unisa.edu.au>
2006-06-05 02:39:25 +0000

    The new 1394cam class files as the old files aren't syntactically valid.

---
+++ src/wcl/1394cam/IEEE1394cam.cpp
@@ -0,0 +1,18 @@
+#include "1394cam.h"
+
+// constructor
+IEEE1394cam::IEEE1394cam()
+{
+
+}
+
+// method to retrieve an image from the camera.
+unsigned char* IEEE1394cam::getImage()
+{
+
+}
+
+// destructor
+IEEE1394cam::~IEEE1394cam()
+{
+}
