57b8ad8e8d95199fc3484e4017f0ea1b684b2916
Aaron Stafford <aaron.stafford@unisa.edu.au>
2006-06-05 02:33:01 +0000

    updated the contructors of this class. added one that accepts a char* and     another (not implemented yet that accepts a struct)

--- src/wcl/1394cam/1394cam.h
+++ src/wcl/1394cam/1394cam.h
@@ -4,8 +4,15 @@
 class 1394cam
 {
 public:
-	// constructor
+	// default constructor
 	1394cam();
+
+	// constructor that takes a char* argument of configuration options
+	1394cam( char* );
+
+	// constructor that takes a struct argument that contains the 
+	// configuration options.
+//	1394cam( some kind of struct );
 
 	// method to retrieve an image from the camera.
 	unsigned char* getImage();
@@ -16,4 +23,5 @@ private:
 	// a pointer to the memory location of the image from the camera
 	unsigned char* imageBuffer;
 };
+
 #endif //_1394CAM_H_
