345335a1b8b7c79ba52f4d103972555bda1606d4
Aaron Stafford <aaron.stafford@unisa.edu.au>
2006-06-15 06:57:59 +0000

    removed the try catch blocks

--- src/wcl/1394cam/osx/OSXIEEE1394Cam.cpp
+++ src/wcl/1394cam/osx/OSXIEEE1394Cam.cpp
@@ -3,11 +3,9 @@
 // constructor
 OSXIEEE1394Cam::OSXIEEE1394Cam()
 {
-	try
-	{
        // create the camera object
-//        this->camera = new cfox::Camera( cfox::Mode_640x480_Mono8, cfox::FPS_30, 0, cfox::BasicCamera );
-        this->camera = new cfox::Camera( cfox::Mode_640x480_RGB, cfox::FPS_30, 0, cfox::BasicCamera );
+        this->camera = new cfox::Camera( cfox::Mode_640x480_Mono8, cfox::FPS_30, 0, cfox::BasicCamera );
+//        this->camera = new cfox::Camera( cfox::Mode_640x480_RGB, cfox::FPS_30, 0, cfox::BasicCamera );
 
         // check that memory was allocated
         if( this->camera == NULL )
@@ -23,11 +21,6 @@ OSXIEEE1394Cam::OSXIEEE1394Cam()
         {
                 gen_fatal( "Camera could not be started, is it plugged in?" );
         }
-	}
-	catch( ... )
-	{
-		gen_fatal( "caught an exception" );
-	}
 }
 
 // constructor that enables the user to enter the 
@@ -41,19 +34,11 @@ OSXIEEE1394Cam::OSXIEEE1394Cam( int width, int height )
 // returns a frame from the camera.
 unsigned char* OSXIEEE1394Cam::getFrame()
 {
-	try
-	{
         // don't know what this does but it was in the example code.
         CFRunLoopRunInMode( kCFRunLoopDefaultMode, 1, true );
         
         // return a frame from the camera.
         return this->camera->getFrame( cfox::MODE_FRAME, 0, 0 );
-	}
-	catch( ... )
-	{
-		gen_fatal( "caught an exception" );
-	}
-	return NULL;
 }
 
 // destructor, doesn't do anything, i'm sure there is stuff that it can do
