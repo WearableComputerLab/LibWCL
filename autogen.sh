c6e73fba1974ddc1ebdd8d843e84a145d3cc386e
Benjamin Close <Benjamin.Close@clearchain.com>
2008-01-11 12:08:23 +1030

    Correctly check version of autotools in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -5,10 +5,6 @@
 #
 #
 #
-ACLOCALMINVERSION="1.10"
-AUTOHEADERMINVERSION="2.61"
-AUTOCONFMINVERSION="2.61"
-
 ACLOCAL=`which aclocal`
 AUTOCONF=`which autoconf`
 AUTOHEADER=`which autoheader`
@@ -18,32 +14,47 @@ AUTOMAKE=`which automake`
 #  ACLOCAL
 #
 
-if test "x$ACLOCAL" = "x"; then
-	echo "You must install aclocal (at least version $ACLOCALMINVERSION)";
-	exit 1
+ACLOCALMINVERSION="1.10"
+acl_version=`${ACLOCAL:-aclocal} --version 2>/dev/null|head -n 1| sed -e 's/^.* \([0-9]\)/\1/' -e 's/[a-z]* *$//' -e 's/\(.*\)\(-p.*\)/\1/'`	
+if test -z "$acl_version"; then	
+  echo "buildconf: aclocal not found."	
+  echo "            You  aclocal version $ACLOCALMINVERSION or newer installed."	
+  exit 1	
+fi	
+IFS=.; set $acl_version; IFS=' '	
+if test "$1" = "1" -a "$2" -lt "10" || test "$1" -lt "1"; then	
+  echo "buildconf: aclocal version $acl_version found."	
+  echo "            You  aclocal version $ACLOCALMINVERSION or newer installed."	
+  echo "            If you have a sufficient aclocal installed, but it"	
+  echo "            is not named 'aclocal', then try setting the"	
+  echo "            aclocal environment variable."	
+  exit 1	
 fi
 
-$ACLOCAL --version | grep $ACLOCALMINVERSION > /dev/null
-if test $? -ne 0; then
-	echo "You version of aclocal is too old, you  at least $ACLOCALMINVERSION";
-	exit 1
-fi
 
 $ACLOCAL  -I cfg
+
+exit 0
 
 #
 # AUTOCONF
 #
 
-if test "x$AUTOCONF" = "x"; then
-	echo "You must install autoconf (at least version $AUTOCONFMINVERSION)";
-	exit 1
-fi
-
-$AUTOCONF --version |grep $AUTOCONFMINVERSION > /dev/null
-if test $? -ne 0; then
-	echo "You version of autoconf is too old, you  at least $AUTOCONFMINVERSION";
-	exit 1;
+AUTOCONFMINVERSION="2.61"
+ac_version=`${AUTOCONF:-autoconf} --version 2>/dev/null|head -n 1| sed -e 's/^.* \([0-9]\)/\1/' -e 's/[a-z]* *$//' -e 's/\(.*\)\(-p.*\)/\1/'`	
+if test -z "$ac_version"; then	
+  echo "buildconf: autoconf not found."	
+  echo "            You  autoconf version $AUTOCONFMINVERSION or newer installed."	
+  exit 1	
+fi	
+IFS=.; set $ac_version; IFS=' '	
+if test "$1" = "2" -a "$2" -lt "61" || test "$1" -lt "2"; then	
+  echo "buildconf: autoconf version $ac_version found."	
+  echo "            You  autoconf version $AUTOCONFMINVERSION or newer installed."	
+  echo "            If you have a sufficient autoconf installed, but it"	
+  echo "            is not named 'autoconf', then try setting the"	
+  echo "            AUTOCONF environment variable."	
+  exit 1	
 fi
 
 $AUTOCONF
@@ -51,15 +62,22 @@ $AUTOCONF
 #
 # AUTOHEADER
 #
-if test "x$AUTOHEADER" = "x"; then
-	echo "You must install autoheader (at least version $AUTOHEADERMINVERSION)";
-	exit 1
-fi
+AUTOHEADERMINVERSION="2.61"
 
-$AUTOHEADER --version |grep $AUTOHEADERMINVERSION > /dev/null
-if test $? -ne 0; then
-	echo "You version of autohead is too old, you  at least $AUTOHEADERMINVERSION";
-	exit 1;
+ah_version=`${AUTOHEADER:-autoheader} --version 2>/dev/null|head -n 1| sed -e 's/^.* \([0-9]\)/\1/' -e 's/[a-z]* *$//' -e 's/\(.*\)\(-p.*\)/\1/'`	
+if test -z "$ah_version"; then	
+  echo "buildconf: autoheader not found."	
+  echo "            You  autoheader version $AUTOHEADERMINVERSION or newer installed."	
+  exit 1	
+fi	
+IFS=.; set $ah_version; IFS=' '	
+if test "$1" = "2" -a "$2" -lt "61" || test "$1" -lt "2"; then	
+  echo "buildconf: autoheader version $ah_version found."	
+  echo "            You  autoheader version $AUTOHEADERMINVERSION or newer installed."	
+  echo "            If you have a sufficient autoheader installed, but it"	
+  echo "            is not named 'autoheader', then try setting the"	
+  echo "            AUTOHEADER environment variable."	
+  exit 1	
 fi
 
 $AUTOHEADER
@@ -67,7 +85,7 @@ $AUTOHEADER
 #
 # AUTOMAKE
 #
-AUTOMAKEMINVERSION="1.9"
+AUTOMAKEMINVERSION="1.10"
 am_version=`${AUTOMAKE:-automake} --version 2>/dev/null|head -n 1| sed -e 's/^.* \([0-9]\)/\1/' -e 's/[a-z]* *$//' -e 's/\(.*\)\(-p.*\)/\1/'`	
 if test -z "$am_version"; then	
   echo "buildconf: automake not found."	
@@ -75,17 +93,13 @@ if test -z "$am_version"; then
   exit 1	
 fi	
 IFS=.; set $am_version; IFS=' '	
-if test "$1" = "1" -a "$2" -lt "9" || test "$1" -lt "1"; then	
+if test "$1" = "1" -a "$2" -lt "10" || test "$1" -lt "1"; then	
   echo "buildconf: automake version $am_version found."	
   echo "            You  automake version $AUTOMAKEMINVERSION or newer installed."	
   echo "            If you have a sufficient automake installed, but it"	
   echo "            is not named 'automake', then try setting the"	
   echo "            AUTOMAKE environment variable."	
   exit 1	
-fi
-if test "x$AUTOMAKE" = "x"; then
-    echo "You must install automake (at least version $AUTOMAKEMINVERSION)";
-    exit 1
 fi
 
 $AUTOMAKE -a
