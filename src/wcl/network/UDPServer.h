da1c98b3df2be61879c2c68f1bd27771042779d2
Benjamin Close <Benjamin.Close@clearchain.com>
2008-09-12 17:10:06 +0930

    Update the copyright for the networking to be libwcl friendly (BSD), add some comments

--- src/wcl/network/UDPServer.h
+++ src/wcl/network/UDPServer.h
@@ -1,17 +1,38 @@
-/**
- * Classes and defines for creating and maintaining networking
+/*-
+ * Copyright (c) 2006-2008 Benjamin Close <Benjamin.Close@clearchain.com>
+ * All rights reserved.
  *
- * $ClearChain: projects/clearchain/src/network/UDPServer.h,v 1.1 2006/05/08 06:03:17 benjsc Exp $
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
- * Copyright (C) 2005 Benjamin Close <Benjamin.Close@clearchain.com>
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
-
 #ifndef WCL_UDPSERVER_H
 #define WCL_UDPSERVER_H
 
 #include <wcl/network/UDPSocket.h>
 #include <wcl/network/UDPPacket.h>
 
+/**
+ * The UDPServer class can be used to listen on a port and 
+ * and receive UDPPackets.
+ */
 class UDPServer: public UDPSocket
 {
     public:
