941e0b85aabc7a311c715e8246c17ab20c305e82
Benjamin Close <Benjamin.Close@clearchain.com>
2010-03-02 13:16:58 +1030

    examples: Update the artoolkit example to show the camera image, accept defines for width, height, use the updated constructor and more

--- examples/artoolkitplus/main.cpp
+++ examples/artoolkitplus/main.cpp
@@ -37,13 +37,51 @@
 #include <wcl/camera/VirtualCamera.h>
 #include <wcl/camera/CameraFactory.h>
 #include <wcl/tracking/ARToolKitPlusTracker.h>
+#define IMAGE_WIDTH 640
+#define IMAGE_HEIGHT 480
 
 using namespace std;
 using namespace wcl;
 
 Camera *cam;
 unsigned char* data;
-ARToolKitPlusTracker tracker(640,480, 50);
+ARToolKitPlusTracker tracker(100,-1,IMAGE_WIDTH,IMAGE_HEIGHT);
+
+/*
+ * OpenGL implementation of glWindowPos*MESA()
+ * (Taken from http://cgit.freedesktop.org/mesa/mesa/src/mesa/main/rastpos.c)
+ */
+void glWindowPos4fMESA( GLfloat x, GLfloat y, GLfloat z, GLfloat w )
+{
+   GLfloat fx, fy;
+
+   /* Push current matrix mode and viewport attributes */
+   glPushAttrib( GL_TRANSFORM_BIT | GL_VIEWPORT_BIT );
+
+   /* Setup projection parameters */
+   glMatrixMode( GL_PROJECTION );
+   glPushMatrix();
+   glLoadIdentity();
+   glMatrixMode( GL_MODELVIEW );
+   glPushMatrix();
+   glLoadIdentity();
+
+   glDepthRange( z, z );
+   glViewport( (int) x - 1, (int) y - 1, 2, 2 );
+
+   /* set the raster (window) position */
+   fx = x - (int) x;
+   fy = y - (int) y;
+   glRasterPos4f( fx, fy, 0.0, w );
+
+   /* restore matrices, viewport and matrix mode */
+   glPopMatrix();
+   glMatrixMode( GL_PROJECTION );
+   glPopMatrix();
+
+   glPopAttrib();
+}
+
 
 double * toGL(const SMatrix &m)
 {
@@ -59,44 +97,15 @@ GLvoid init()
 {
 	glShadeModel(GL_SMOOTH);
 	glClearColor(0,0,0,0);
-	//glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LEQUAL);
 
-	data = new unsigned char[640*480*3];
-
-	// create a texture for the image...
-	glTexImage2D(GL_TEXTURE_2D,             //target
-				 0,                         //level of detail
-				 3,                         //colour components
-				 640,                       //width
-				 480,                       //height
-				 0,                         //border
-				 GL_RGB,                    //image format
-				 GL_UNSIGNED_BYTE,
-				 data);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
-
-	//glEnable(GL_TEXTURE_2D);
-
-//	glMatrixMode(GL_PROJECTION);
-//	glLoadMatrixf(tracker.getProjectionMatrix());
-	//gluOrtho2D(0,1,0,1);
-	glMatrixMode(GL_MODELVIEW);
+	data = new unsigned char[IMAGE_WIDTH*IMAGE_HEIGHT*3];
 }
 
 GLvoid reshape(int width, int height)
 {
 	glViewport(0,0, (GLsizei)width, (GLsizei)height);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective( 45.0, width/(GLfloat)height, 1.0f, 5000.0f );
-//	glLoadMatrixf(tracker.getProjectionMatrix());
-	//glLoadMatrixd(toGL(tracker.getProjectionMatrix()));
-	//gluOrtho2D(0,1,0,1);
-	glMatrixMode(GL_MODELVIEW);
 }
 
 void keyboard(unsigned char key, int w, int h)
@@ -112,50 +121,39 @@ GLvoid display()
 	const unsigned char* frame = cam->getFrame();
 	switch(cam->getImageFormat()){
 	    case Camera::YUYV422:
-		Camera::convertImageYUV422toRGB8(frame, data, 640, 480);
+		Camera::convertImageYUV422toRGB8(frame, data, IMAGE_WIDTH, IMAGE_HEIGHT);
 		break;
 	    case Camera::MONO8:
-		Camera::convertImageMONO8toRGB8(frame,data,640,480);
+		Camera::convertImageMONO8toRGB8(frame,data, IMAGE_WIDTH, IMAGE_HEIGHT);
 		break;
 	    case Camera::RGB8:
 	    default:
 		memcpy(data,frame,cam->getFormatWidth() * cam->getFormatHeight() * 3);
 	}
 
-	glTexSubImage2D(GL_TEXTURE_2D,
-	                0,
-					0,
-					0,
-					640,
-					480,
-					GL_RGB,
-					GL_UNSIGNED_BYTE,
-					data);
+
+	// Flip the image, glPixelZoom should do this but for some reason
+	// I can't get it to work
+	unsigned i=0;
+	for(unsigned rows=IMAGE_HEIGHT; rows > IMAGE_HEIGHT/2; rows--){
+	    for(unsigned cols=0; cols < IMAGE_WIDTH*3; cols++){
+		int source = (rows*IMAGE_WIDTH*3) + cols;
+		int dest = (i * IMAGE_WIDTH*3) + cols;
+		unsigned char c = data[dest];
+		data[dest]=data[source];
+		data[source]=c;
+	    }
+	    i++;
+	}
+
+	glWindowPos4fMESA(1.0, 1.0, 1.0, 1.0);
+	glDrawPixels( IMAGE_WIDTH, IMAGE_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, data );
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-//	gluPerspective( 45.0, width/(GLfloat)height, 1.0f, 5000.0f );
-	glLoadMatrixf(tracker.getProjectionMatrix());
+	glLoadMatrixd(toGL(tracker.getProjectionMatrix()));
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	//glTranslatef(0.0,0.0,-500.0);
-	//glRotatef(180.0, 0.0, 1.0, 0.0);
-
-	glColor3f(1,0,0);
-	glBegin(GL_QUADS);
-
-		glTexCoord2f(0,1);
-		glVertex2f(0,0);
-
-		glTexCoord2f(1,1);
-		glVertex2f(1,0);
-
-		glTexCoord2f(1,0);
-		glVertex2f(1,1);
-
-		glTexCoord2f(0,0);
-		glVertex2f(0,1);
-	glEnd();
 
 	// Find objects in the scene and render a cube on it
 	tracker.update();
@@ -163,33 +161,20 @@ GLvoid display()
 	vector<TrackedObject *> objects = tracker.getAllObjects();
 	glDisable(GL_TEXTURE_2D);
 
-	glLoadMatrixf(tracker.getModelViewMatrix());
-	glColor3f(0.0,1.0, 0.0);
-	glutSolidCube(40.0);
-	    /*
 	for(vector<TrackedObject *>::iterator it = objects.begin();
 	    it != objects.end();
 	    ++it ){
 
 	    TrackedObject *object = *it;
-	    glPushMatrix();
 	    SMatrix m(4);
-	    
+	    cout << object->toString();
+
 	    m = object->getTransform();
-	    for(int c=0; c < 3; c++ ){
-		for( int r = 0; r < 3; r++ )
-		    m[c][r]=0;
-	    }
-	    m.print();
-
-	    //glTranslatef(0.0, 0.0, 25.0);
-	    glutSolidCube(1.0);
-	    glPopMatrix();
-	    break;
+	    glLoadMatrixd(toGL(m));
+	    glColor3f(1.0,1.0, 0.0);
+	    glutSolidCube(40.0);
 	}
-	    */
 
-	//glEnable(GL_TEXTURE_2D);
 	glFlush();
 	glutSwapBuffers();
 
@@ -203,31 +188,21 @@ GLvoid display()
 
 int main(int argc, char** argv)
 {
-    /*
-    CameraBuffer b;
-    FILE *f ;
-    f = fopen("image_320_240_8_marker_id_bch_nr0100.raw", "rb");
-    unsigned char *buffer = (unsigned char *)malloc(320*240);
-    fread(buffer,1, 320*240, f );
-    fclose(f);
-    b.start = buffer;
-    b.length=320*240;
-
-    VirtualCamera *c = new VirtualCamera();
-    c->setFrames(&b,1);
-    c->setFormat(Camera::MONO8, 320,240);
-    */
-
     try {
     cam = CameraFactory::getCamera();
-    cam->setFormat(Camera::MONO8, 640, 480);
+    if( cam == NULL ){
+	printf("No Cameras are available\n");
+	return 1;
+    }
+
+    cam->setFormat(Camera::MONO8, IMAGE_WIDTH, IMAGE_HEIGHT);
     cam->printDetails();
 
     tracker.setCamera(cam);
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(640, 480);
+    glutInitWindowSize(IMAGE_WIDTH, IMAGE_HEIGHT);
     glutInitWindowPosition(100,100);
     glutCreateWindow(argv[0]);
     init();
