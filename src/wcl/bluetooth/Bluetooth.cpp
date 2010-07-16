0424d663eebed6cb6e40382cf4edd4c109d8842f
Michael Marner <marnermr@cs.unisa.edu.au>
2010-07-16 13:44:10 +0930

    Method for scanning for bluetooth devices.

--- src/wcl/bluetooth/Bluetooth.cpp
+++ src/wcl/bluetooth/Bluetooth.cpp
@@ -19,182 +19,233 @@
 
 #include "Bluetooth.h"
 
-Bluetooth::Bluetooth() 
- : fd(0), connected(false)
+
+namespace wcl
 {
-}
 
-Bluetooth::~Bluetooth(){
-}
-
-int Bluetooth::connect(std::string destmac, bool blocking){
-
-	this->destmac = destmac;
-
-	/*Socket Address*/
-	struct sockaddr_rc addr = {0};
-
-	/*Get the socket file descriptor*/
-	fd = socket(PF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
-
-	if(fd<=0){
-		std::cerr << ("Unable to create socket %s", destmac.c_str());
-		return false;
+	Bluetooth::Bluetooth() 
+		: fd(0), connected(false)
+	{
 	}
 
-	/*Get the current socket flags*/
-	int flags = fcntl (fd, F_GETFL, 0);
-	if (flags < 0){
-		std::cerr << ("Error retrieving file descriptor flags\n");
-		return false;
+	Bluetooth::~Bluetooth(){
 	}
 
-	/*If we want to use a non blocking connect*/
-	if(!blocking){
-		if (fcntl (fd, F_SETFL, flags | O_NONBLOCK) < 0){
-			std::cerr << ("Failed to set non blocking mode\n");
+	int Bluetooth::connect(std::string destmac, bool blocking){
+
+		this->destmac = destmac;
+
+		/*Socket Address*/
+		struct sockaddr_rc addr = {0};
+
+		/*Get the socket file descriptor*/
+		fd = socket(PF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+
+		if(fd<=0){
+			std::cerr << ("Unable to create socket %s", destmac.c_str());
 			return false;
 		}
+
+		/*Get the current socket flags*/
+		int flags = fcntl (fd, F_GETFL, 0);
+		if (flags < 0){
+			std::cerr << ("Error retrieving file descriptor flags\n");
+			return false;
+		}
+
+		/*If we want to use a non blocking connect*/
+		if(!blocking){
+			if (fcntl (fd, F_SETFL, flags | O_NONBLOCK) < 0){
+				std::cerr << ("Failed to set non blocking mode\n");
+				return false;
+			}
+		}
+
+		/**/
+		addr.rc_family = AF_BLUETOOTH;
+
+		/*Set the channel? have seen it set to 0 in some examples*/
+		addr.rc_channel = (uint8_t) 1;
+
+		/*set the connection parameters (who to connect to)*/
+		str2ba( destmac.c_str(), &addr.rc_bdaddr );
+
+		int state = ::connect(fd, (struct sockaddr *)&addr, sizeof(addr));
+
+		return state;
 	}
 
-	/**/
-	addr.rc_family = AF_BLUETOOTH;
+	int Bluetooth::readRaw(std::string &buffer, int bytes){
 
-	/*Set the channel? have seen it set to 0 in some examples*/
-	addr.rc_channel = (uint8_t) 1;
+		/*allocate the memory for the read call*/
+		char data [bytes];
+		int result = read (fd, data, bytes);
 
-	/*set the connection parameters (who to connect to)*/
-	str2ba( destmac.c_str(), &addr.rc_bdaddr );
+		/*Nuke anything in the buffer if no data was read*/
+		if (result <= 0)
+			buffer.erase ();
 
-	int state = ::connect(fd, (struct sockaddr *)&addr, sizeof(addr));
+		/*Set the string to the data read*/
+		else
+			buffer.assign (data, result);
 
-	return state;
-}
-
-int Bluetooth::readRaw(std::string &buffer, int bytes){
-
-	/*allocate the memory for the read call*/
-	char data [bytes];
-	int result = read (fd, data, bytes);
-
-	/*Nuke anything in the buffer if no data was read*/
-	if (result <= 0)
-		buffer.erase ();
-
-	/*Set the string to the data read*/
-	else
-		buffer.assign (data, result);
-
-	return result;
-}
-
-
-
-bool Bluetooth::setBlocking(bool blocking){
-
-	/*Get the current socket flags*/
-	int flags = fcntl (fd, F_GETFL, 0);
-	if (flags < 0){
-		std::cerr << ("Error retrieving file descriptor flags\n");
-		return false;
+		return result;
 	}
 
-	int state = false;
 
-	/*Set blocking mode*/
-	if(blocking)
-		flags &= ~O_NONBLOCK;
-	/*Set non blocking mode*/
-	else
-		flags |= O_NONBLOCK;
 
-	if (fcntl (fd, F_SETFL, flags) < 0)
-		state = false;
+	bool Bluetooth::setBlocking(bool blocking){
 
-	return state;
-}
+		/*Get the current socket flags*/
+		int flags = fcntl (fd, F_GETFL, 0);
+		if (flags < 0){
+			std::cerr << ("Error retrieving file descriptor flags\n");
+			return false;
+		}
 
-bool Bluetooth::reConnect(bool blocking){
-	close();
-	return connect(destmac, blocking);
-}
+		int state = false;
 
-/*Lets us know how much data is waiting on the socket, also a -1 occurs if the
- * device has been disconnected.*/
-int Bluetooth::dataAvailable(){
-	int readmask = 0;
+		/*Set blocking mode*/
+		if(blocking)
+			flags &= ~O_NONBLOCK;
+		/*Set non blocking mode*/
+		else
+			flags |= O_NONBLOCK;
 
-	fd_set rfds;
-	struct timeval tv;
-	FD_ZERO(&rfds);
-	FD_SET(getFD(), &rfds);
-	tv.tv_sec = 0;
-	tv.tv_usec = 1;
-	int nfound;
-	//if ((nfound = select (getFD()+1, &rfds, 0, 0, &tv)) == -1){
-	/*
-	   message("Sellecting...\n");
-	   if ((nfound = select (getFD(), &rfds, 0, 0, &tv)) == -1){
-	   std::cout << "Select failed" << std::endl;
-	   }
-	   else{
-	   message("NFound: %d\n", nfound);
-	   */
-	//message("Here...\n");
-	//if (FD_ISSET(getFD(), &rfds)){
-	//std::cout << "Ready for reading: " << nfound << std::endl;
-	int MAX_BUF = 128;
-	char buf[MAX_BUF];
-	//int length = recv(getFD(), &buf, 128, 0);
-	int recvFlags =
-		MSG_PEEK;      /*Return data without removing it from the queue*/
-	//| MSG_TRUNC;  /*Return the real length of the packet even if longer than the buffer*/
-	int length = recv(getFD(), buf, MAX_BUF, 0);
-	//std::cerr << "Ready for reading: " << length << std::endl;
-	return length;
+		if (fcntl (fd, F_SETFL, flags) < 0)
+			state = false;
 
-	/*Detect a disconnect*/
-	if(length == 0){
-		return 0;
+		return state;
 	}
-	else{
+
+	bool Bluetooth::reConnect(bool blocking){
+		close();
+		return connect(destmac, blocking);
+	}
+
+	/*Lets us know how much data is waiting on the socket, also a -1 occurs if the
+	 * device has been disconnected.*/
+	int Bluetooth::dataAvailable(){
+		int readmask = 0;
+
+		fd_set rfds;
+		struct timeval tv;
+		FD_ZERO(&rfds);
+		FD_SET(getFD(), &rfds);
+		tv.tv_sec = 0;
+		tv.tv_usec = 1;
+		int nfound;
+		//if ((nfound = select (getFD()+1, &rfds, 0, 0, &tv)) == -1){
+		/*
+		   message("Sellecting...\n");
+		   if ((nfound = select (getFD(), &rfds, 0, 0, &tv)) == -1){
+		   std::cout << "Select failed" << std::endl;
+		   }
+		   else{
+		   message("NFound: %d\n", nfound);
+		   */
+		//message("Here...\n");
+		//if (FD_ISSET(getFD(), &rfds)){
+		//std::cout << "Ready for reading: " << nfound << std::endl;
+		int MAX_BUF = 128;
+		char buf[MAX_BUF];
+		//int length = recv(getFD(), &buf, 128, 0);
+		int recvFlags =
+			MSG_PEEK;      /*Return data without removing it from the queue*/
+		//| MSG_TRUNC;  /*Return the real length of the packet even if longer than the buffer*/
+		int length = recv(getFD(), buf, MAX_BUF, 0);
+		//std::cerr << "Ready for reading: " << length << std::endl;
 		return length;
+
+		/*Detect a disconnect*/
+		if(length == 0){
+			return 0;
+		}
+		else{
+			return length;
+		}
+
+		//}
+		//else
+		//  return 0;
+		//  }
+
 	}
 
-	//}
-	//else
-	//  return 0;
-	//  }
+	int Bluetooth::close(){
+		return ::close(this->fd);
+	}
 
-}
+	int Bluetooth::writeChar(char *c){
+		return write(this->fd, c, 1);
+	}
 
-int Bluetooth::close(){
-	return ::close(this->fd);
-}
+	int Bluetooth::writeLine(std::string line){
+		int result =  ::write(this->fd, line.c_str(), line.size());
+		return (bool)result;
+	}
 
-int Bluetooth::writeChar(char *c){
-	return write(this->fd, c, 1);
-}
+	bool Bluetooth::flush(){
 
-int Bluetooth::writeLine(std::string line){
-	int result =  ::write(this->fd, line.c_str(), line.size());
-	return (bool)result;
-}
+		std::string buf;
+		int result = readRaw(buf, 1024*64);
+		if(result > 0)
+			return true;
+		else
+			return false;
+	}
 
-bool Bluetooth::flush(){
+	int Bluetooth::getFD(){ 
+		return this->fd; 
+	}
 
-	std::string buf;
-	int result = readRaw(buf, 1024*64);
-	if(result > 0)
-		return true;
-	else
-		return false;
-}
+	std::string Bluetooth::getName(){
+		return this->devName;
+	}
 
-int Bluetooth::getFD(){ 
-	return this->fd; 
-}
+	}
 
-std::string Bluetooth::getName(){
-	return this->devName;
-}
+	std::vector<BluetoothDevice> Bluetooth::scanForDevices()
+	{
+		std::vector<BluetoothDevice> devices;
+
+		inquiry_info *ii = NULL;
+		int max_rsp, num_rsp;
+		int dev_id, sock, len, flags;
+		int i;
+		char addr[19] = { 0 };
+		char name[248] = { 0 };
+
+		dev_id = hci_get_route(NULL);
+		sock = hci_open_dev( dev_id );
+		if (dev_id < 0 || sock < 0) {
+			perror("opening socket");
+			exit(1);
+		}
+
+		len  = 8;
+		max_rsp = 255;
+		flags = IREQ_CACHE_FLUSH;
+		ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
+
+		num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
+		if( num_rsp < 0 ) perror("hci_inquiry");
+
+		for (i = 0; i < num_rsp; i++) {
+			ba2str(&(ii+i)->bdaddr, addr);
+			memset(name, 0, sizeof(name));
+			if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name), 
+						name, 0) < 0)
+				strcpy(name, "[unknown]");
+			printf("%s  %s\n", addr, name);
+			BluetoothDevice d(name, addr);
+			devices.push_back(d);
+		}
+
+		free( ii );
+		close( sock );
+
+		return devices;
+	}
+
+} // namespace
--- src/wcl/bluetooth/Bluetooth.h
+++ src/wcl/bluetooth/Bluetooth.h
@@ -15,33 +15,53 @@
 
 #include <string>
 
-class Bluetooth {
-	private:
-		int fd;
-		bool connected;
-		std::string devName;
-		std::string destmac;
 
-	protected:
+namespace wcl
+{
 
-	public:
-		Bluetooth();
-		virtual ~Bluetooth();
+	class Bluetooth {
+		public:
 
-		/*IODevice Sigs*/
-		int connect(std::string dev, bool blocking);
-		bool reConnect(bool blocking);
-		int close();
-		int readRaw(std::string&, int bytes);
-		int dataAvailable();
-		int writeLine(std::string line);
-		int writeChar(char* c);
-		bool setBlocking(bool state);
-		bool flush();
-		int getFD();
-		std::string getName();
-		//char * scan();
-};
+			struct BluetoothDevice
+			{
+				std::string name;
+				std::string mac;
+
+				BluetoothDevice() {}
+
+				BluetoothDevice(std::string n, std::string m)
+					: name(n), mac(m) {}
+			};
+
+			Bluetooth();
+			virtual ~Bluetooth();
+
+
+			static std::vector<BluetoothDevice> scanForDevices();
+
+			/*IODevice Sigs*/
+			int connect(std::string dev, bool blocking);
+			bool reConnect(bool blocking);
+			int close();
+			int readRaw(std::string&, int bytes);
+			int dataAvailable();
+			int writeLine(std::string line);
+			int writeChar(char* c);
+			bool setBlocking(bool state);
+			bool flush();
+			int getFD();
+			std::string getName();
+			//char * scan();
+			
+		private:
+			int fd;
+			bool connected;
+			std::string devName;
+			std::string destmac;
+
+	};
+
+}
 #endif
 
 
