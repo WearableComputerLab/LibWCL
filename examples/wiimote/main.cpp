353781280a292d802e4a128f40fd516b73416aa9
Michael Marner <marnermr@cs.unisa.edu.au>
2010-10-15 13:36:40 +1030

    Fixed comments (removed references to vicon), added BSD header.

--- examples/wiimote/main.cpp
+++ examples/wiimote/main.cpp
@@ -1,11 +1,34 @@
+/*-
+ * Copyright (c) 2010 Michael Marner <michael@20papercups.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+
+
 /*
- * ViconSync.
- *
- * This is a small utility to record the offset matrix from the Vicon System.
- * This is so we can use the vicon tracking data with the projectors.
- *
+ * Wiimote Example.
  * @author Michael Marner (marnermr@cs.unisa.edu.au)
- *
  */
 
 
@@ -148,12 +171,6 @@ int main(int argc, char** argv)
 			}
 
 			mvprintw(coordPos.y+11, coordPos.x,   "Raw Button State %s", ss.str().c_str());
-					/*
-			mvprintw(coordPos.y+1, coordPos.x, "% 10.3f % 10.3f % 10.3f % 10.3f", m[1][0], m[1][1], m[1][2], m[1][3]);
-			mvprintw(coordPos.y+2, coordPos.x, "% 10.3f % 10.3f % 10.3f % 10.3f", m[2][0], m[2][1], m[2][2], m[2][3]);
-			mvprintw(coordPos.y+3, coordPos.x, "% 10.3f % 10.3f % 10.3f % 10.3f", m[3][0], m[3][1], m[3][2], m[3][3]);
-			*/
-
 			wiimote->update();
 			refresh();
 		}
