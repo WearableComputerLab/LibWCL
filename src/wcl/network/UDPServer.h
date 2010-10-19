f83285955b433f7091636a8b34e4a084f89c9200
Benjamin Close <Benjamin.Close@clearchain.com>
2010-10-19 16:12:00 +1030

    network: Add multicast support. Multicast is UDP + a multicast group. The UDP client side is normal UDP, the UDP Server side needs the multicast support

--- src/wcl/network/UDPServer.h
+++ src/wcl/network/UDPServer.h
@@ -39,7 +39,7 @@ namespace wcl {
 class WCL_API UDPServer: public UDPSocket
 {
     public:
-	UDPServer( const unsigned port ) throw (SocketException);
+	UDPServer( const unsigned port, const char *mcastgroup = NULL ) throw (SocketException);
 };
 
 
