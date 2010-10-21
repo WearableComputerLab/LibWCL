366590d8fc235a7fb6153321057e3dc45869c9d6
Benjamin Close <Benjamin.Close@clearchain.com>
2010-10-21 10:55:30 +1030

    examples: Update the udp network examples to actually use exceptions

--- examples/network/udpclient.cpp
+++ examples/network/udpclient.cpp
@@ -1,17 +1,24 @@
 #include <stdio.h>
+#include <iostream>
 #include "wcl/network/UDPSocket.h"
 
 using namespace wcl;
+using namespace std;
 
 int main(void)
 {
     char buffer[4096];
 
-    UDPSocket s("127.0.0.1", 55555);
-    s.write("Hello", 5 );
-    s.read((char *)buffer, 4096);
-    printf("Client: Server Said: %s\n", buffer);
-    s.close();
+    try {
+	UDPSocket s("127.0.0.1", 55555);
+	s.write("Hello", 5 );
+	s.read((char *)buffer, 4096);
+	printf("Client: Server Said: %s\n", buffer);
+	s.close();
+    } catch (SocketException e)
+    {
+	cout << e.what() << endl;
+    }
 
     return 0;
 }
--- examples/network/udpserver.cpp
+++ examples/network/udpserver.cpp
@@ -1,22 +1,30 @@
+#include <iostream>
 #include <stdio.h>
 #include "wcl/network/UDPServer.h"
 
 using namespace wcl;
+using namespace std;
 
 int main(void)
 {
-    UDPServer s(55555);
-    UDPPacket p(5);
-    s.setBlockingMode( Socket::NONBLOCKING);
-    
-    while( true ){
-	if ( s.read( &p ) > 0 ){
-	    printf("Server:Got a packet\n");
-	    s.write( &p );
-	}
-	printf("No packet received, sleeping\n");
-	sleep(1);
-    }
+	UDPServer s(55555);
+	UDPPacket p(5);
+	s.setBlockingMode(Socket::NONBLOCKING);
 
-    return 0;
+	while( true ){
+	    try {
+		if ( s.read( &p ) > 0 ){
+		    printf("Server:Got a packet\n");
+		    s.write( &p );
+		}
+	    }
+
+	    catch (SocketException e )
+	    {
+		cout << e.what() << endl;
+	    }
+	    printf("No packet received, sleeping\n");
+	    sleep(1);
+	}
+	return 0;
 }
