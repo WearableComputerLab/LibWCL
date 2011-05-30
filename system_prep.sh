d068e4191ecd9ccd4de9eff0adfbed3dd708e41a
Tim Simon <tim.m.simon@gmail.com>
2011-05-30 18:47:02 +0930

    - Added the last of the packages  - Adds the ARToolKitPlus repo to, and removes from, /etc/apt/sources.list

--- system_prep.sh
+++ system_prep.sh
@@ -7,6 +7,20 @@
 #   to get by without it. 
 # This script also assumes that you have seperatly installed ARToolKitPlus. 
 
+
+# Add the ARToolKitPlus repo to our sources. 
+# Copy, replace etc the file because otherwise it'll get run multipule times
+# And that'll append multipule repos
+# And that's probably bad. Better way to do this would be to check if repo's in 
+# sources list, but that's alot of effort. Feel free to fix. 
+
+cp /etc/apt/sources.list /etc/apt/sources.list.backup
+echo '' >> /etc/apt/sources.list
+echo '# ARToolKitPlus repository' >> /etc/apt/sources.list
+echo 'deb http://ppa.launchpad.net/michael-the-drummer/wcl/ubuntu lucid main' >> /etc/apt/sources.list
+
+apt-get update
+
 apt-get install \
 build-essential \
 automake \
@@ -17,4 +31,8 @@ libdc1394-22-dev \
 libavcodec-dev \
 libavformat-dev \
 libswscale-dev \
+artoolkitplus-dev \
+libcwiid-dev 
 
+# Replace the sources.list with the backup
+mv /etc/apt/sources.list.backup /etc/apt/sources.list
