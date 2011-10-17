f4d50f7703782b994980becbf4164b282bb014b0
Tim Simon <tim.simon@radiumblue.net>
2011-10-17 21:44:11 +1030

    Added license header and authoring information; added checks for argument validity.

--- examples/projectors/pcontrol.cpp
+++ examples/projectors/pcontrol.cpp
@@ -1,3 +1,31 @@
+/*
+ * Copyright (c) <2011>, Tim Simon <tim.simon@radiumblue.net>
+ * All rights reserved.
+ * 
+ *  Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are 
+ * met:
+ * 
+ *  Redistributions of source code must retain the above copyright notice, 
+ * this list of conditions and the following disclaimer.
+ * 
+ *  Redistributions in binary form must reproduce the above copyright 
+ * notice, this list of conditions and the following disclaimer in the 
+ * documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <cstdlib>
 #include <string>
 #include <iostream>
@@ -30,6 +58,8 @@ int main(int argc, char** argv) {
 
     std::string address = "127.0.0.1";    
     unsigned int port = 7142;
+
+    // Determine whethe the arguments given are valid. 
     if ( argc >= 2 ) {
 
         for ( int counter = 0; counter < argc ; ++counter ) {
@@ -62,6 +92,16 @@ int main(int argc, char** argv) {
             }
         }
 
+        // Now determine whether we actually got given a command to send to the projector or not. 
+        if (( strcmp (argv[argc-1], "--ON" ) == 0 ) ||  ( strcmp (argv[argc-1], "--OFF" ) == 0 ) ) {
+        } else {
+            usage();
+            return -1;
+        }
+                // 02H 01H 00H 00H 00H 03H 
+                unsigned char toSend[] = { (unsigned char)2, (unsigned char)1, 0, 0, 0, (unsigned char)3 }; 
+                projector.write((char*) toSend, 6); 
+            }
         try {
 
             TCPSocket projector(address,port);
