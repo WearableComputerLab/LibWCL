f993594e3183293bdf1202c1753ad7932eaa005e
Tim Simon <tim.simon@radiumblue.net>
2011-10-17 15:04:44 +1030

    Added ON / OFF commands, removed segfaults.

--- examples/projectors/pcontrol.cpp
+++ examples/projectors/pcontrol.cpp
@@ -8,7 +8,7 @@ using namespace wcl;
 
 void usage() {
     printf("NAME\n\tpcontrol - Network command controller for NEC projectors\n\n"
-            "SYNOPSIS\n\t pcontrol [options] -a <address> -p <port>\n\n"
+            "SYNOPSIS\n\t pcontrol [options] -a <address> -p <port> [COMMAND]\n\n"
             "When no options, address or port are specified, the defaults of:\n"
             "127.0.0.1:7142\n"
             "are used.\n\n"
@@ -19,16 +19,20 @@ void usage() {
             "\t\tThe address of the projector to communicate with.\n"
             "\t-p\n\t--port\n"
             "\t\tThe port to communicate on.\n"
-    );
+            "COMMANDS\n[COMMAND] can be any single value of the following:"
+            "\t--ON\nPower ON\n"
+            "\t--OFF\nPower OFF\n"
+          );
 }
+
 
 int main(int argc, char** argv) {
 
-    std::string address = "";    
-    unsigned int port = 0;
+    std::string address = "127.0.0.1";    
+    unsigned int port = 7142;
     if ( argc >= 2 ) {
 
-        for ( int counter = 0; counter <= argc ; ++counter ) {
+        for ( int counter = 0; counter < argc ; ++counter ) {
 
             if ( strcmp ( argv[counter], "--help" ) == 0 || strcmp ( argv[counter], "-h" ) == 0 ) {
                 usage();
@@ -57,24 +61,32 @@ int main(int argc, char** argv) {
                 }   
             }
         }
-   } else {
-       usage();
-       return -1;
+
+        try {
+
+            TCPSocket projector(address,port);
+            // Get the last value, which should be our command, and use it for our command to run. 
+            if ( strcmp (argv[argc-1], "--ON" ) == 0 ) {
+                // 02H 00H 00H 00H 00H 00H 02H
+                unsigned char toSend[] = { (unsigned char)2, 0, 0, 0, 0, (unsigned char)2 }; 
+                projector.write((char*) toSend, 6);
+            }
+            if ( strcmp (argv[argc-1], "--OFF" ) == 0 ) {
+
+                // 02H 01H 00H 00H 00H 03H 
+                unsigned char toSend[] = { (unsigned char)2, (unsigned char)1, 0, 0, 0, (unsigned char)3 }; 
+                projector.write((char*) toSend, 6); 
+            }
+            // Read the response from the projector. 
+            unsigned char buffer[4096];
+            projector.read((char *)buffer, 4096);
+            std::cout << "Projector responded: " << (int)buffer[0] << "," << buffer[1] << std::endl; 
+            projector.close();
+        } catch (SocketException* e) {
+            fprintf( stderr, "%s", e->what() ); 
+        }
+
+        }
+
+        return 0;
     }
-    
-    
-    unsigned char buffer[4096];
-
-    TCPSocket projector(address,port);
-
-    // We want to write: 02H 00H 00H 00H 00H 00H 02H
-    // to the projector. Converting this to decimal. 
-    unsigned char toSend[] = { (unsigned char)2, 0, 0, 0, 0, (unsigned char)2 }; 
-    projector.write((char*) toSend, 6);
-
-    projector.read((char *)buffer, 4096);
-    std::cout << "Projector responded: " << (int)buffer[0] << "," << buffer[1] << std::endl; 
-    projector.close();
-    return 0;
-
-}
