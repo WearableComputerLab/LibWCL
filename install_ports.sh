8e0c1ee9ea7fb533a24ad2f59123b4da3f7e7ada
Michael Marner <michael@20papercups.net>
2014-06-10 15:37:58 +0930

    Installs ports needed for LibWCL on OSX.
     o Assumes you already have MacPorts working
     o Assumes you already have XCode console tools working.

---
+++ install_ports.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo port install autoconf \
+                  libtool \
+                  automake \
+                  autoconf \
+                  pkgconfig \
+                  ffmpeg-devel \
+                  libdc1394 \
+                  flex 
