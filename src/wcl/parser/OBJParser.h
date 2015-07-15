895081e5c227ec955884a47b260d35d129f47a84
Michael Marner <michael.marner@unisa.edu.au>
2015-07-15 12:00:18 +0930

    Remove Flex/Yacc from the build.

--- src/wcl/parser/OBJParser.h
+++ src/wcl/parser/OBJParser.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2009 Benjamin Close <Benjamin.Close@clearchain.com>
+ * Copyright (c) 2015 Michael Marner <michael@20papercups.net>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -28,89 +28,24 @@
 
 #include <iostream>
 #include <stack>
-#include <wcl/parser/Parser.h>
 #include <wcl/geometry/OBJGeometry.h>
 
 
 namespace wcl {
 
-// Forward declare the FlexScanner needed for the stack in this class
-class OBJFormatScanner;
+    /**
+     * An OBJParser that understands the .obj File format. The parser
+     * builds up a geometry object consisting of all elements of the OBJ
+     * file format. This can be obtained via the call to getGeometry().
+     * The memory for this object is maintained by the OBJParser. Destroying
+     * the class instance frees up the memory associated with the Geometry
+     */
+    class WCL_API OBJParser
+    {
+        public:
+            wcl::OBJGeometry* parse(const std::string& filename);
 
-/**
- * An OBJParser that understands the .obj File format. The parser
- * builds up a geometry object consisting of all elements of the OBJ
- * file format. This can be obtained via the call to getGeometry().
- * The memory for this object is maintained by the OBJParser. Destroying
- * the class instance frees up the memory associated with the Geometry
- */
-class WCL_API OBJParser: public Parser
-{
-    public:
-	/**
-	 * Callback Function supported by the parser to translate a relative path
-	 * into a fully qualified path
-	 *
-	 * @param relpath The relative path
-	 * @return The fully qualified path
-	 */
-	typedef const std::string (*RelativeToAbsolute)(const std::string &);
-
-        OBJParser(std::istream &, RelativeToAbsolute=NULL);
-        ~OBJParser();
-
-        void parse() throw (ParserException);
-        void print();
-
-        const OBJGeometry &getGeometry();
-
-        void addMaterialLibrary( const std::string &);
-        void addMaterial(const std::string &);
-        void addGroup(const std::string &);
-        void addVertex(const double, const double, const double);
-        void addVertexTexture(const double, const double);
-        void addNormal(const double, const double, const double);
-	void newFace();
-	void addFaceVertex(const double, const double, const double);
-        void useMaterial(const std::string &);
-        void setMaterialDiffuse(const double, const double, const double);
-        void setMaterialAmbience(const double, const double, const double);
-        void setMaterialSpecular(const double, const double, const double);
-        void setMaterialOpacity(const double, const double, const double);
-        void setMaterialRefractionIndex( const double );
-        void setMaterialSpecularExponent(const double );
-	void setMaterialEmissive(const double, const double, const double);
-        void setMaterialIlluminationGroup( const int );
-        void setSmoothingGroup( const std::string &);
-        void setMaterialDiffuseMap(const std::string &);
-        void setMaterialAmbientMap(const std::string &);
-        void setMaterialSpecularMap(const std::string &);
-        void setMaterialAlphaMap(const std::string &);
-        void setMaterialBumpMap(const std::string &);
-
-	int getLineNo();
-	const char* getLine();
-
-        static int scanner(OBJParser *);
-
-	// We expose this so yacc can use it
-	using Parser::parseError;
-
-    private:
-	RelativeToAbsolute func;
-        std::istream *input;
-        std::stack<OBJFormatScanner *> stack;
-        OBJGeometry data;
-        OBJMaterial *material;
-        OBJGroup *group;
-	OBJSmoothing *smoothing;
-	OBJFace *face;
-
-        OBJParser(const OBJParser &); /* NIMP */
-
-        bool hasGroup();
-        bool hasMaterial();
-};
+    };
 
 };
 
