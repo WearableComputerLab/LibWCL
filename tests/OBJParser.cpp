05ec3302aad3d27464d68e37cef0a595c044f9c5
Michael Marner <michael@20papercups.net>
2015-07-16 10:28:18 +0930

    Test OBJParser line types.

--- tests/OBJParser.cpp
+++ tests/OBJParser.cpp
@@ -8,10 +8,21 @@ class OBJParserTest : public ::testing::Test {
 };
 
 
+// parse should throw an exception if the file could not be opened.
 TEST(OBJParserTest, FileNotFound) {
     wcl::OBJParser p;
     EXPECT_THROW(p.parse("filedoesnotexist.obj"), wcl::ParserException);
 }
     
+
+TEST(OBJParserTest, GetLineType) {
+    wcl::OBJParser p;
+
+    EXPECT_EQ(wcl::OBJParser::VERTEX, p.getLineType("v"));
+    EXPECT_EQ(wcl::OBJParser::NORMAL, p.getLineType("n"));
+    EXPECT_EQ(wcl::OBJParser::TEX_COORD, p.getLineType("vt"));
+    EXPECT_EQ(wcl::OBJParser::FACE, p.getLineType("f"));
+    EXPECT_EQ(wcl::OBJParser::USE_MTL, p.getLineType("usemtl"));
+}
 
 
